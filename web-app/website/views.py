c33df1d658473ee167c61fd5085c728ec1a0d020
Mahmoud El Bergui <46870458+MaHmOuDeB@users.noreply.github.com>
2021-10-28 20:33:36 +0200

    Update views.py

--- web-app/website/views.py
+++ web-app/website/views.py
@@ -33,6 +33,114 @@ def song():
             return render_template('song.htm')
     return render_template('song.htm')
 
+
+@views.route('/artist', methods=['POST', 'GET'])
+def artist():  
+    if request.method == 'POST':
+        artist_name = request.form.get('name')
+
+
+        if artist_name:
+            record = Artist(artist_name)
+            db.session.add(record)
+            db.session.commit()
+
+            flash(f"{record} sucessfully added!", category='success')
+            return redirect(url_for('views.home'))  #if input was correct go back to maintenance page
+        else:                                      # so create a route for maintenance too btw
+            return render_template('artist.htm')
+    return render_template('artist.htm')
+
+@views.route('/band', methods=['POST', 'GET'])
+def band():  
+    if request.method == 'POST':
+        band_name = request.form.get('name')
+        band_form_year = request.form.get('year')
+
+        if band_name and band_form_year:
+            record = Band(band_name, band_form_year)
+            db.session.add(record)
+            db.session.commit()
+
+            flash(f"{record} sucessfully added!", category='success')
+            return redirect(url_for('views.home'))  #if input was correct go back to maintenance page
+        else:                                      # so create a route for maintenance too btw
+            return render_template('band.htm')
+    return render_template('band.htm')
+
+
+
+@views.route('/singer', methods=['POST', 'GET'])
+def singer():  
+    if request.method == 'POST':
+        singer_name = request.form.get('name')
+        singer_birth_year = request.form.get('year')
+
+        if singer_name and singer_birth_year:
+            record = Singer(singer_name, singer_birth_year)
+            db.session.add(record)
+            db.session.commit()
+
+            flash(f"{record} sucessfully added!", category='success')
+            return redirect(url_for('views.home'))  #if input was correct go back to maintenance page
+        else:                                      # so create a route for maintenance too btw
+            return render_template('singer.htm')
+    return render_template('singer.htm')
+
+
+@views.route('/album', methods=['POST', 'GET'])
+def album():  
+    if request.method == 'POST':
+        numTracks = request.form.get("number")
+        album_name = request.form.get('name')
+
+        if numTracks and album_name:
+            record = Album(numTracks, album_name)
+            db.session.add(record)
+            db.session.commit()
+
+            flash(f"{record} sucessfully added!", category='success')
+            return redirect(url_for('views.home'))  #if input was correct go back to maintenance page
+        else:                                      # so create a route for maintenance too btw
+            return render_template('album.htm')
+    return render_template('album.htm')
+
+
+
+
+@views.route('/genre', methods=['POST', 'GET'])
+def genre():  
+    if request.method == 'POST':
+        genre_name = request.form.get('name')
+
+        if genre_name:
+            record = Genre(genre_name)
+            db.session.add(record)
+            db.session.commit()
+
+            flash(f"{record} sucessfully added!", category='success')
+            return redirect(url_for('views.home'))  #if input was correct go back to maintenance page
+        else:                                      # so create a route for maintenance too btw
+            return render_template('genre.htm')
+    return render_template('genre.htm')
+
+
+@views.route('/genre', methods=['POST', 'GET'])
+def rating():  
+    if request.method == 'POST':
+        stars = request.form.get('stars')
+
+        if stars:
+            record = Rating(stars)
+            db.session.add(record)
+            db.session.commit()
+
+            flash(f"{record} sucessfully added!", category='success')
+            return redirect(url_for('views.home'))  #if input was correct go back to maintenance page
+        else:                                      # so create a route for maintenance too btw
+            return render_template('rating.htm')
+    return render_template('rating.htm')
+
 def check():  # just error checking if the database connected
     try:
         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
